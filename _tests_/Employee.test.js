8e69874b575443ebb37fc7a572e70b0affcf3164
syntaxsemantixs <gormeno2030@gmail.com>
2023-01-24 01:46:33 -0500

    start tests

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,37 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it(' should show the contstructor values for the employee object', () => {
+            const employee = Employee('Gerardo', '123456789', 'gerardo1234@gmail.com');
+
+            expect(employee.name).toBe("Gerardo");
+            expect(employee.id).toBe("123456789");
+            expect(employee.email).toBe("gerardo1234@gmail.com");
+        });
+
+        it(' should get the name from the getName() method', () => {
+            const employee = Employee('Gerardo', '123456789', 'gerardo1234@gmail.com');
+
+            expect(employee.getName()).toBe("Gerardo");
+        });
+
+        it(' should get the id from the getId() method', () => {
+            const employee = Employee('Gerardo', '123456789', 'gerardo1234@gmail.com');
+
+            expect(employee.getId()).toBe("123456789");
+        });
+
+        it(' should get the email from the getEmail() method', () => {
+            const employee = Employee('Gerardo', '123456789', 'gerardo1234@gmail.com');
+
+            expect(employee.getEmail()).toBe("gerardo1234@gmail.com");
+        });
+
+        it(' should get the Role from the getRole() method', () => {
+            const employee = Employee('Gerardo', '123456789', 'gerardo1234@gmail.com');
+
+            expect(employee.getEmail()).toBe("Employee");
+        });
+    })
+});
